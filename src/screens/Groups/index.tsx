5b4e3827c97850375e51c104603c87c1a84e8ad7
Vagner Reis <vagnereix.dev@gmail.com>
2024-02-04 10:47:54 -0300

    feat: add empty state component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,25 @@ import { Header } from '@/components/Header';
 import { Container } from './styles';
 import { Highlight } from '@/components/Highlight';
 import { GroupCard } from '@/components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@/components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-      <GroupCard title='Group' onPress={() => console.log('press group')} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item, _) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+        contentContainerStyle={!groups.length && { flex: 1 }}
+      />
     </Container>
   );
 }
