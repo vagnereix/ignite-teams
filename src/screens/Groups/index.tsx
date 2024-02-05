36cff2b2c3eb48351ef0a45866cccfb09f9bc330
Vagner Reis <vagnereix.dev@gmail.com>
2024-02-04 22:34:04 -0300

    feat: add navigation types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,9 +6,16 @@ import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@/components/ListEmpty';
 import { Button } from '@/components/Button';
+import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const { navigate } = useNavigation();
+
+  function handleNewTeam() {
+    navigate('new');
+  }
 
   return (
     <Container>
@@ -26,7 +33,7 @@ export function Groups() {
         contentContainerStyle={!groups.length && { flex: 1 }}
       />
 
-      <Button title='Criar novo time' />
+      <Button title='Criar novo time' onPress={handleNewTeam} />
     </Container>
   );
 }
