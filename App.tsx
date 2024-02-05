f477807fce66acfde57ab46027fafaa4d27562c9
Vagner Reis <vagnereix.dev@gmail.com>
2024-02-04 22:23:41 -0300

    feat: add app routes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Groups } from '@/screens/Groups';
 import { ThemeProvider } from 'styled-components/native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 import theme from '@/theme';
 
 import { Loading } from '@/components/Loading';
-import { NewGroup } from '@/screens/NewGroup';
-import { Players } from '@/screens/Players';
+import { Routes } from '@/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_700Bold,
   });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar style="inverted" />
-      {fontsLoaded ? <Players /> : <Loading />}
+      <StatusBar style='inverted' />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
